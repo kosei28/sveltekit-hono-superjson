b70b1b965467d95f5f6830320a610bf87017c9fa
kosei28 <niwa.kosei@gmail.com>
2023-05-07 13:32:16 +0900

    fix hono handler

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,12 @@
 import type { Handle } from '@sveltejs/kit';
 import { app } from '$lib/hono';
-import { Hono } from 'hono';
 
 export const handle: Handle = async ({ event, resolve }) => {
     if (event.url.pathname.startsWith('/api')) {
-        if (event.url.pathname.endsWith('/')) {
-            event.url.pathname = event.url.pathname.slice(0, -1);
-            const req = new Request(event.url.toString(), event.request);
-            event.request = req;
-        }
+        event.url.pathname = event.url.pathname.slice(4);
+        event.request = new Request(event.url.toString(), event.request);
 
-        return await new Hono().route('/api', app).handleEvent(event);
+        return await app.handleEvent(event);
     }
 
     return resolve(event);
